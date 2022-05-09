4aaeaff8406530e4f235c06b590bca8e34602f5d
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-05-09 14:45:04 -0700

    boilerplate

---
+++ shopify-inventory-api/src/main/java/com/services/ProductService.java
@@ -0,0 +1,19 @@
+package com.services;
+
+import com.models.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> findAll();
+
+    Product findProductById(long id);
+
+    void delete(long id);
+
+    Product save(Product product);
+
+    Product update(
+            Product product,
+            long id);
+}
