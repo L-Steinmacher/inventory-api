c4e798466edc195fd3b76eee9877b5397f4d79e2
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-05-16 16:11:55 -0700

    git

--- inventory-api/src/main/java/com/repository/OrderDetailsRepository.java
+++ inventory-api/src/main/java/com/repository/OrderDetailsRepository.java
@@ -5,9 +5,14 @@ import com.models.OrderDetailsId;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OrderDetailsRepository extends CrudRepository<OrderDetails, OrderDetailsId> {
 
     @Query(value = "SELECT * FROM ORDERDETAILS \n" +
             "WHERE productid = :productId")
     OrderDetails findByProductId(Long productId);
+
+    List<OrderDetails> findAll();
+
 }
