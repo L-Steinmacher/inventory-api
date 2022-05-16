b77d345049a0adfb953a49dc98e773519508134a
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2022-05-16 16:15:43 -0700

    Remove duplicated directory

--- shopify-inventory-api/src/main/java/com/repository/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import com.models.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductsRepository
-        extends CrudRepository<Product, Long> {
-}
